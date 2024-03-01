8d63ea4cbfab62d49e376ed97f1ef8d94d29ea8a
Maria Istrate <mistrate06@gmail.com>
2024-03-01 15:14:06 +0200

    initial commit

---
+++ blaket/src/App.js
@@ -0,0 +1,17 @@
+import logo from './blackcat_logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          MEDIA PORTFOLIO
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
